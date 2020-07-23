39633c55aa7e91fc7391afb8769ac667002aecde
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2020-07-23 17:25:06 -0400

    file structure

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <p>
-          EVENTSInc
-        </p>
+        <p>EVENTSInc</p>
 
       </header>
     </div>
